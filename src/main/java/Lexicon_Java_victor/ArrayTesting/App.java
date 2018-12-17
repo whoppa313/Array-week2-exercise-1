5e969c6e411b7cd633559cb27e28f3ff41f79dc8
Victor_506 <viho98001@utb.vaxjo.se>
2018-12-17 10:25:35 +0100

    first

---
+++ src/main/java/Lexicon_Java_victor/ArrayTesting/App.java
@@ -0,0 +1,20 @@
+package Lexicon_Java_victor.ArrayTesting;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    	
+    	int[] Array = new int[3];
+    	Array[0]= 11;
+    	Array[1]= 23;
+    	Array[2]= 39;
+    	System.out.println(Array[0]);
+    	System.out.println(Array[1]);
+    	System.out.println(Array[2]);
+    	
+    	
+    }
+}
