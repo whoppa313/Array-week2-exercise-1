027b246b2d2a918ef898a73efd5cbd2c88d87f03
Victor_506 <viho98001@utb.vaxjo.se>
2019-01-14 15:02:27 +0100

    .

--- src/main/java/Lexicon_Java_victor/ArrayTesting/App.java
+++ src/main/java/Lexicon_Java_victor/ArrayTesting/App.java
@@ -1,12 +1,22 @@
 package Lexicon_Java_victor.ArrayTesting;
 
+import Java_Lexicon.Workshopv2.RandomNumberGen;
 
 public class App 
 {
     public static void main( String[] args )
     {
 
+    	//first try with the number generator jar works.
     	
+    	RandomNumberGen rng = RandomNumberGen.getInsance();
+    	System.out.println(rng.getRandomIntBetween(13, 43));
+    	
+    	
+    }
+    
+    private void stuff()
+    {
     	int[] Array = new int[3];
     	Array[0]= 11;
     	Array[1]= 23;
@@ -14,7 +24,5 @@ public class App
     	System.out.println(Array[0]);
     	System.out.println(Array[1]);
     	System.out.println(Array[2]);
-    	
-    	
     }
 }
